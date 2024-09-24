17702160dc359c03324a2fea6c3b1c38dc4113f5
Oliver Yuan <yxy02oliver@outlook.com>
2024-09-24 19:30:13 -0400

    possible problem

--- modelTest/try2.py
+++ modelTest/try2.py
@@ -8,8 +8,8 @@ red_wine_path = 'winequality-red-corrected.csv'
 white_wine_path = 'winequality-white-corrected.csv'
 red_wine_data = pd.read_csv(red_wine_path)
 white_wine_data = pd.read_csv(white_wine_path)
-crop_data = pd.read_csv('WinnipegDataset.csv')
-bc_data = pd.read_csv('wdbc.csv')
+# crop_data = pd.read_csv('WinnipegDataset.csv')
+# bc_data = pd.read_csv('wdbc.csv')
 
 red_wine_data['target'] = (red_wine_data['quality'] >= 8).astype(int)
 X_red = red_wine_data.drop(columns=['quality', 'target']).values
@@ -17,21 +17,21 @@ y_red = red_wine_data['target'].values
 white_wine_data['target'] = (white_wine_data['quality'] >= 8).astype(int)
 X_white = white_wine_data.drop(columns=['quality', 'target']).values
 y_white = white_wine_data['target'].values
-bc_data['target'] = (bc_data['diagnosis'] == 'M').astype(int)
-X_bc = bc_data.drop(columns=['diagnosis', 'target']).values
-y_bc = bc_data['target'].values
+# bc_data['target'] = (bc_data['diagnosis'] == 'M').astype(int)
+# X_bc = bc_data.drop(columns=['diagnosis', 'target']).values
+# y_bc = bc_data['target'].values
 
-crop_random_sample_list = random.sample(range(325835), 10000)
-crop_data = crop_data.iloc[crop_random_sample_list]
-crop_data['target'] = (crop_data['label'] == 6).astype(int)
-X_crop = crop_data.drop(columns=['label', 'target']).values
-y_crop = crop_data['target'].values
+# crop_random_sample_list = random.sample(range(325835), 10000)
+# crop_data = crop_data.iloc[crop_random_sample_list]
+# crop_data['target'] = (crop_data['label'] == 6).astype(int)
+# X_crop = crop_data.drop(columns=['label', 'target']).values
+# y_crop = crop_data['target'].values
 
 # randon sample
 X_red_sample, _, y_red_sample, _ = train_test_split(X_red, y_red, test_size=0.5, random_state=42)
 X_white_sample, _, y_white_sample, _ = train_test_split(X_white, y_white, test_size=0.5, random_state=42)
-X_crop_sample, _, y_crop_sample, _ = train_test_split(X_crop, y_crop, test_size=0.5, random_state=42)
-X_bc_sample, _, y_bc_sample, _ = train_test_split(X_bc, y_bc, test_size=0.5, random_state=42)
+# X_crop_sample, _, y_crop_sample, _ = train_test_split(X_crop, y_crop, test_size=0.5, random_state=42)
+# X_bc_sample, _, y_bc_sample, _ = train_test_split(X_bc, y_bc, test_size=0.5, random_state=42)
 
 # Adjust Lambda
 def compute_lambda(n, theta):
@@ -66,8 +66,10 @@ def wide_reach_classification(X, y, dataset_name, theta, epsilon_R=0.01, epsilon
         name="precision_constraint"
     )
 
+
+    # !!!might have problem here
     for i in range(num_samples):
-        if y[i] == 1:  #P
+        if y[i] == 0.5:  #P
             model.addConstr(x[i] <= 1 + sum(w[k] * X[i, k] for k in range(num_features)) - c - epsilon_P, name=f"classification_positive_{i}")
         else:  #N
             model.addConstr(y_vars[i] >= sum(w[k] * X[i, k] for k in range(num_features)) - c + epsilon_N, name=f"classification_negative_{i}")
@@ -100,10 +102,10 @@ def wide_reach_classification(X, y, dataset_name, theta, epsilon_R=0.01, epsilon
         }
 
 results = []
-results.append(wide_reach_classification(X_bc_sample, y_bc_sample, "B&C", theta=0.99))
+# results.append(wide_reach_classification(X_bc_sample, y_bc_sample, "B&C", theta=0.99))
 results.append(wide_reach_classification(X_red_sample, y_red_sample, "Wine Quality (red)", theta=0.04))
 results.append(wide_reach_classification(X_white_sample, y_white_sample, "Wine Quality (white)", theta=0.1))
-results.append(wide_reach_classification(X_crop_sample, y_crop_sample, "Crop", theta=0.9))
+# results.append(wide_reach_classification(X_crop_sample, y_crop_sample, "Crop", theta=0.9))
 df_results = pd.DataFrame(results)
 print("Summary of Results:")
 print(df_results.to_string(index=False))
