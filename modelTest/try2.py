dfd4f57ad1178d335614a0843fc658aef0dbb432
Rickylin888 <sxl1813@case.edu>
2024-09-25 15:59:43 -0400

    revised the algortihm in lagrangian

--- modelTest/try2.py
+++ modelTest/try2.py
@@ -4,10 +4,10 @@ from gurobipy import Model, GRB, quicksum
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-red_wine_data = pd.read_csv('winequality-red-corrected.csv')
-white_wine_data = pd.read_csv('winequality-white-corrected.csv')
-crop_data = pd.read_csv('WinnipegDataset.csv')
-bc_data = pd.read_csv('wdbc.csv')
+red_wine_data = pd.read_csv('modelTest\winequality-red-corrected.csv')
+white_wine_data = pd.read_csv('modelTest\winequality-white-corrected.csv')
+#crop_data = pd.read_csv('WinnipegDataset.csv')
+#bc_data = pd.read_csv('wdbc.csv')
 
 # data processing
 red_wine_data['target'] = (red_wine_data['quality'] >= 8).astype(int)
@@ -18,21 +18,21 @@ white_wine_data['target'] = (white_wine_data['quality'] >= 8).astype(int)
 X_white = white_wine_data.drop(columns=['quality', 'target']).values
 y_white = white_wine_data['target'].values
 
-bc_data['target'] = (bc_data['diagnosis'] == 'M').astype(int)
-X_bc = bc_data.drop(columns=['diagnosis', 'target']).values
-y_bc = bc_data['target'].values
+#bc_data['target'] = (bc_data['diagnosis'] == 'M').astype(int)
+#X_bc = bc_data.drop(columns=['diagnosis', 'target']).values
+#y_bc = bc_data['target'].values
 
 crop_random_sample_list = random.sample(range(325835), 10000)
-crop_data = crop_data.iloc[crop_random_sample_list]
-crop_data['target'] = (crop_data['label'] == 6).astype(int)
-X_crop = crop_data.drop(columns=['label', 'target']).values
-y_crop = crop_data['target'].values
+#crop_data = crop_data.iloc[crop_random_sample_list]
+#crop_data['target'] = (crop_data['label'] == 6).astype(int)
+#X_crop = crop_data.drop(columns=['label', 'target']).values
+#y_crop = crop_data['target'].values
 
 # randon sample
-X_red_sample, _, y_red_sample, _ = train_test_split(X_red, y_red, test_size=0.5, random_state=42)
-X_white_sample, _, y_white_sample, _ = train_test_split(X_white, y_white, test_size=0.5, random_state=42)
-X_crop_sample, _, y_crop_sample, _ = train_test_split(X_crop, y_crop, test_size=0.5, random_state=42)
-X_bc_sample, _, y_bc_sample, _ = train_test_split(X_bc, y_bc, test_size=0.5, random_state=42)
+X_red_sample, _, y_red_sample, _ = train_test_split(X_red, y_red, test_size=0.5, random_state=50)
+X_white_sample, _, y_white_sample, _ = train_test_split(X_white, y_white, test_size=0.5, random_state=50)
+#X_crop_sample, _, y_crop_sample, _ = train_test_split(X_crop, y_crop, test_size=0.5, random_state=42)
+#X_bc_sample, _, y_bc_sample, _ = train_test_split(X_bc, y_bc, test_size=0.5, random_state=42)
 
 
 # Adjust Lambda
@@ -62,9 +62,9 @@ def wide_reach_classification(X, y, dataset_name, theta, epsilon_R=0.01, epsilon
     V = model.addVar(vtype=GRB.CONTINUOUS, name="V")
 
     # Apply  Lagrangian
-    model.setObjective(quicksum(x[i] for i in range(num_samples)) - lambda_value * V, GRB.MAXIMIZE)
+    model.setObjective(quicksum((x[i] if y[i] ==1 else 0) for i in range(num_samples)) - lambda_value * V, GRB.MAXIMIZE)
     model.addConstr(
-        V >= (theta - 1) * quicksum(x[i] for i in range(num_samples)) + theta * quicksum(y_vars[j] for j in range(num_samples)) + theta * epsilon_R,
+        V >= (theta - 1) * quicksum((x[i] if y[i] ==1 else 0) for i in range(num_samples)) + theta * quicksum((y_vars[j] if y[j] ==0 else 0)for j in range(num_samples)) + theta * epsilon_R,
         name="precision_constraint"
     )
 
@@ -83,7 +83,7 @@ def wide_reach_classification(X, y, dataset_name, theta, epsilon_R=0.01, epsilon
         initial_reach = sum(1 for i in range(num_samples) if y[i] == 1)
         bc_reach = sum(x[i].X for i in range(num_samples) if x[i].X > 0.5)
         nodes = model.NodeCount
-
+        model.write('output.sol')
         print(f"{dataset_name} Dataset Results:")
         print(f"Initial Reach: {initial_reach}")
         print(f"BC Reach: {bc_reach}")
@@ -105,10 +105,10 @@ def wide_reach_classification(X, y, dataset_name, theta, epsilon_R=0.01, epsilon
         }
 
 results = []
-results.append(wide_reach_classification(X_bc_sample, y_bc_sample, "B&C", theta=0.99))
-results.append(wide_reach_classification(X_red_sample, y_red_sample, "Wine Quality (red)", theta=0.04))
-results.append(wide_reach_classification(X_white_sample, y_white_sample, "Wine Quality (white)", theta=0.1))
-results.append(wide_reach_classification(X_crop_sample, y_crop_sample, "Crop", theta=0.9))
+#results.append(wide_reach_classification(X_bc_sample, y_bc_sample, "B&C", theta=0.99))
+results.append(wide_reach_classification(X_red, y_red, "Wine Quality (red)", theta=0.04))
+results.append(wide_reach_classification(X_white, y_white, "Wine Quality (white)", theta=0.1))
+#results.append(wide_reach_classification(X_dcrop_sample, y_crop_sample, "Crop", theta=0.9))
 
 df_results = pd.DataFrame(results)
 print("Summary of Results:")
